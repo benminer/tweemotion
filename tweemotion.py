564b1b3e1f2e36a8e80339df743e73e34ed7e943
Ben Miner <ben.miner@pop.belmont.edu>
2018-10-23 13:45:09 -0500

    removed tensorflow, added keras. started data cleanup

--- tweemotion.py
+++ tweemotion.py
@@ -1,5 +1,22 @@
 import util
 
-data = util.readCsv('./data/text_emotion_full.csv', columns=['sentiment', 'content'])
+data = util.readCsv('./data/text_emotion_full.csv')
+array = util.toList(data)
 
-print(data)
+sentimentAndContent = []
+
+# 1 is sentiment, 3 is content
+for item in array:
+    sentimentAndContent.append({'sentiment': item[1], 'content': item[3]})
+
+
+for item in sentimentAndContent:
+    content = item['content']
+    if 'the' in content:
+        content = content.replace('the', '')
+    if 'a' in content:
+        content = content.replace('a', '')
+    item['content'] = content
+
+#
+print(sentimentAndContent)
--- util.py
+++ util.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
 
-def readCsv(filePath, columns):
-    return pd.read_csv(filePath, names=columns)
+def readCsv(filePath):
+    return pd.read_csv(filePath, header=1)
+
+
+def toList(data):
+    return pd.np.array(data)
