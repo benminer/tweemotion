c57fe813c6c5d9cf8fabdf5cfd00429a5967fbd8
Nick Chouard <nick.chouard@gmail.com>
2018-10-28 16:37:45 -0500

    functions to keep only usable sentiments

--- util.py
+++ util.py
@@ -1,12 +1,34 @@
 import pandas as pd
+import csv
 
 
 def readCsv(filePath):
-    return pd.read_csv(filePath, header=1)
+    data = []
+    with open(filePath) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data.append({'sentiment': row['sentiment'], 'content': row['content']})
+    return data
 
 
-def toList(data):
-    return pd.np.array(data)
+def removeUnusedSentiments(data):
+    sentiments = {}
+    for row in data:
+        sentiment = row['sentiment']
+        if sentiment in sentiments.keys():
+            sentiments[sentiment] += 1
+        else:
+            sentiments[sentiment] = 1
+    print(sentiments)
+    sentiments_list = []
+    for key, value in sentiments.items():
+        if value > 1000:
+            sentiments_list.append(key)
+    new_data = []
+    for row in data:
+        if row['sentiment'] in sentiments_list:
+            new_data.append(row)
+    return new_data
 
 
 def cleanString(str):
@@ -20,3 +42,7 @@ def cleanString(str):
     if 'while' in str:
         val = str.replace('while', '')
     return val
+
+if __name__ == '__main__':
+    data = readCsv('./data/text_emotion_full.csv')
+    print(removeUnusedSentiments(data))
